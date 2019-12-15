aca37e7791f2da4da63902f5e1a9ee094838ef53
ja4ari <ja4ari@gmail.com>
2019-12-15 18:05:46 +0330

    finance.py tasks has been completed.

--- finance.py
+++ finance.py
@@ -1,5 +1,36 @@
+from datetime import datetime
+
+
 # TODO-1: Add Bill class here
+class Bill:
+    def __init__(self, uuid, total_price, payment):
+        self.uuid = uuid
+        self.total_price = total_price
+        self.payment = payment
+
+    @classmethod
+    def sample(cls):
+        return cls(
+            uuid=3001, total_price=107000, payment=Payment.sample()
+        )
+
+
 # TODO-1: Add Payment class here
+class Payment:
+    def __init__(self, uuid, payment_type, datetime, price):
+        self.uuid = uuid
+        self.payment_type = payment_type
+        self.__is_paid = False
+        self.datetime = datetime
+        self.price = price
+
+    @classmethod
+    def sample(cls):
+        return cls(
+            uuid=4001, payment_type='Cash',
+            datetime=datetime.fromisoformat('2011-11-04T00:05:23'),
+            price=107000)
+
 # TODO-1: Add .sample() classmethod for Bill and Payment which returns
 # an instance:
 # for example:
