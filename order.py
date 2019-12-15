2bd3b6aeef95e5dfde01e16ead9719d6cddac752
ja4ari <ja4ari@gmail.com>
2019-12-15 17:44:05 +0330

    order.py tasks has been completed.

--- order.py
+++ order.py
@@ -1,5 +1,23 @@
+from datetime import datetime
+
+
 # TODO-1: Add Order model here
+class Order:
+    def __init__(self, uuid, item_dict, in_out, datetime, bill, table):
+        self.uuid = uuid
+        self.Item_dict = item_dict
+        self.in_out = in_out
+        self.datetime = datetime
+        self.bill = bill
+        self.table = table
+
 # TODO-1: Add .sample() classmethod for Order which returns an instance:
+    @classmethod
+    def sample(cls):
+        return cls(
+            uuid=2001, item_dict={'peperoni pizza': 1}, in_out='in',
+            datetime=datetime.fromisoformat('2011-11-04T00:05:23'), bill=None, table=None
+        )
 # for example:
 #    class Test:
 #         def __init__(self, name, number):
