fb3985d19724769d0a8cee595fafea9990ac20f4
ja4ari <ja4ari@gmail.com>
2019-12-15 16:11:43 +0330

    user.py and menu.py tasks has been completed.

--- menu.py
+++ menu.py
@@ -1,5 +1,16 @@
 # TODO-1: Add Item class here
+class Item:
+    def __init__(self, uuid, name, item_type, price, time):
+        self.uuid = uuid
+        self.name = name
+        self.item_type = item_type,
+        self.price = price
+        self.time = time
+
 # TODO-1: Add .sample() classmethod for Item which returns an instance:
+    @classmethod
+    def sample(cls):
+        return cls(uuid=1001, name='peperoni pizza', item_type='Food', price=25000, time=25)
 # for example:
 #    class Test:
 #         def __init__(self, name, number):
