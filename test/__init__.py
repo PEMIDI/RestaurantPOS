1ab6ced93232b923091c933e55ccce081a345c1b
Hosein <hs.ramezanpoor@gmail.com>
2019-12-27 20:08:01 +0330

    Modify test package and make relative imports

---
+++ test/__init__.py
@@ -0,0 +1,7 @@
+from test.step_1 import CheckAllClassDefinition
+from test.step_2 import TestDiscountClass, TestOrderClass, TestItemClass, \
+    ConstructorValidatorTest
+from test.step_3 import CommonClassChecks, MenuClassChecks, FinanceChecks
+
+__all__ = ['CheckAllClassDefinition', 'TestDiscountClass', 'TestOrderClass', 'TestItemClass', "CommonClassChecks",
+           'MenuClassChecks', "FinanceChecks"]
