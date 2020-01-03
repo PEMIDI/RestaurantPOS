9b73e223f5b6b60aacb51ee6d92fa977a6546f2e
Hosein <hs.ramezanpoor@gmail.com>
2020-01-03 13:08:00 +0330

    Fix step 4 test bugs (pass typo on todos)

--- test/step_4.py
+++ test/step_4.py
@@ -3,4 +3,8 @@ import unittest
 
 class CheckFunctionExistence(unittest.TestCase):
     def test_step_functions_existence(self):
-        from utils import create_time, get_order, show_unpaid_bill, pay_bill, get_finance_report
+        from utils import get_order, show_unpaid_bill, pay_bill, get_finance_report
+        try:
+            from utils import create_time
+        except:
+            from utils import create_item
